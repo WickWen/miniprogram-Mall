ae125c86d5896dd726222c9b5f89e8ddf15f2937
WickWen <mrwickweb@163.com>
2020-04-05 02:04:23 +0800

    数据渲染和样式细节处理

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,18 +1,56 @@
-// pages/goods_detail/index.js
+import { axios } from '../../request/promise_axios'
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    pics: [],
+    goods_price:5999,
+    goods_name:'',
+    goods_introduce: ''
+  },
+  
+  // 获取商品详情请求
+  getGoodsDetail() {
+    const { goods_id } = this.options
+    axios({
+      url: '/goods/detail',
+      data: {
+        goods_id
+      }
+    })
+    .then(res => {
+      let { pics, goods_price, goods_name, goods_introduce } = res
+      console.log(res);
+      
+      this.setData({
+        pics,
+        goods_price,
+        goods_name,
+        goods_introduce
+      })
+    })
+  },
 
+  // 预览图片
+  viewImage(e) {
+    const { current } = e.currentTarget.dataset
+    // 获取到的数组处理成字符串数组
+    const urls = this.data.pics.map(item => item.pics_big);
+    wx.previewImage({
+      current, // 当前显示图片的http链接
+      urls    // 需要预览的图片http链接列表   格式 Array.<string>
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // console.log(options);
+    this.getGoodsDetail()
   },
 
   /**
